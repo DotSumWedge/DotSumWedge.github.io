4980891dbe46fe3919b5b11640f4d8ea8b73b481
Derek Rogers <derekgrogers1@gmail.com>
2022-11-08 16:32:24 -0600

    Updating styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import BarChartPage from './pages/BarChartPage';
 import DerekRogersPhoto from './Images/Derek_Professional_Photo.jpg';
 
 function App() {
+
   return (
     <div className="App" style={appDivStyle}>
         <div style={{ width: 200, height: 10 }}> </div>
@@ -12,22 +12,23 @@ function App() {
           Derek Rogers
         </h1>
         <img src={DerekRogersPhoto} style={{ width: 200, height: 200 }}/>
-        <h2>
-          From: Overland Park, Kansas
-        </h2>
-        <h2>
-          Interests: Outside of studies and work I enjoy a variety of hobbies including disc golf, snowboarding, rock climbing, games, painting/modeling, building computers, playing piano/guitar, listening to music, and cooking. I find joy in trying something new and watching people dedicated to their craft show off their skills and knowledge.
-        </h2>
-        <h2>
-          Work: Software Developer at Argo Data
-        </h2>
-        <h2>
-          School: BS of Computer Science and Minor in mathematics at the University of North Texas and currently pursuing  a MS in Data Science at Southern Methodist University
-        </h2>
+
+        <h2> From:</h2>
+        <div>Overland Park, Kansas</div>
+
+        <h2>Interests:</h2>
+        <div>Outside of studies and work I enjoy a variety of hobbies including disc golf, snowboarding, rock climbing, games, painting/modeling, building computers, playing piano/guitar, listening to music, and cooking. I find joy in trying something new and watching people dedicated to their craft show off their skills and knowledge.</div>
+        
+        <h2>Work:</h2>
+        <div>Software Developer at Argo Data</div>
+
+        <h2>School:</h2>
+        <div>BS of Computer Science and Minor in mathematics at the University of North Texas and currently pursuing  a MS in Data Science at Southern Methodist University</div>
+
+        <div style={{ width: 200, height: 20 }}> </div>
         <Routes>
             <Route path="*" element={<BarChartPage />} />
         </Routes>
-
     </div>
   );
 }
