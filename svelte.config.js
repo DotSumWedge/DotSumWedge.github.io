b2649e4593990b64af4be760afeaf5568004b285
Derek Rogers <derekgrogers1@gmail.com>
2024-06-01 10:54:21 -0500

    Added configurion for github pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,25 @@
-import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. you don't need to specify these
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			trailingSlash: 'always'
+		}),
+		paths: {
+			base: dev ? '' : '/webApp',
+		}
 	},
-}
+};
 
 export default config;
